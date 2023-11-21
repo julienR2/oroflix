1cab4abf9bc5f9b58674dda285ddb8c343f45ad0
julienr2 <julien.rougeron@gmail.com>
2023-11-21 09:24:12 +0000

    started Video controls

--- src/components/MediaVideo.tsx
+++ src/components/MediaVideo.tsx
@@ -3,57 +3,79 @@ import { ororoApi } from '../libs/ororoApi'
 import { Episode } from '../types/ororo'
 import { useOnKeyPress } from '../hooks/useOnKeyPress'
 import { useMediaStore } from '../hooks/useMediaStore'
+import {
+  FocusContext,
+  useFocusable,
+} from '@noriginmedia/norigin-spatial-navigation'
 
-export const MediaVideoComponent = () => {
-  const { playingShow, playingMovie, setStoreItem } = useMediaStore()
-  const [episode, setEpisode] = React.useState<Episode>()
-  const isHidden = !playingMovie && (!playingShow || !episode)
+import { ReactComponent as PlayIcon } from '../assets/play.svg'
+import { ReactComponent as PauseIcon } from '../assets/pause.svg'
 
-  const onClose = React.useCallback(() => {
-    setStoreItem('playingMovie', undefined)
-    setStoreItem('playingShow', undefined)
-  }, [setStoreItem])
+export const MediaVideoComponent = React.forwardRef<HTMLDivElement, {}>(
+  (props, ref) => {
+    const { playingShow, playingMovie, setStoreItem } = useMediaStore()
+    const [episode, setEpisode] = React.useState<Episode>()
+    const isHidden = !playingMovie && (!playingShow || !episode)
 
-  useOnKeyPress({ onBack: onClose, skip: isHidden })
+    const onClose = React.useCallback(() => {
+      setStoreItem('playingMovie', undefined)
+      setStoreItem('playingShow', undefined)
+    }, [setStoreItem])
 
-  React.useEffect(() => {
-    async function fetchDetails() {
-      if (!playingShow) return
+    useOnKeyPress({ onBack: onClose, skip: isHidden })
 
-      const data = await ororoApi.episode(playingShow.episodeId)
+    React.useEffect(() => {
+      async function fetchDetails() {
+        if (!playingShow) return
 
-      setEpisode(data)
+        const data = await ororoApi.episode(playingShow.episodeId)
+
+        setEpisode(data)
+      }
+
+      fetchDetails()
+    }, [playingShow])
+
+    if (isHidden) {
+      return null
     }
 
-    fetchDetails()
-  }, [playingShow])
-
-  if (isHidden) {
-    return null
-  }
-
-  return (
-    <div className="fixed w-screen h-screen bg-black z-30 flex justify-center left-0">
-      <video
-        id="video"
-        src={(episode || playingMovie)?.download_url}
-        width="100%"
-        height="100%"
-        poster={(playingShow || playingShow)?.backdrop_url}
-        preload="auto"
-        autoPlay
-        controls
-      />
-    </div>
-  )
-}
+    return (
+      <div
+        ref={ref}
+        className="fixed w-screen h-screen bg-black z-30 flex justify-center left-0">
+        <video
+          id="video"
+          src={(episode || playingMovie)?.download_url}
+          width="100%"
+          height="100%"
+          poster={(playingShow || playingShow)?.backdrop_url}
+          preload="auto"
+          // autoPlay
+          controls
+        />
+        <div className="bg-black bg-opacity-60 absolute top-0 left-0 right-0 bottom-0 z-20 flex flex-col justify-between">
+          <div className="bg-gray-950 bg-opacity-90 h-[15vh]"></div>
+          <div className="bg-gray-950 bg-opacity-90 h-[15vh] flex items-center px-24">
+            <PlayIcon height="50%" />
+          </div>
+        </div>
+      </div>
+    )
+  },
+)
 
 export const MediaVideo = () => {
   const { playingMovie, playingShow } = useMediaStore()
+  const { ref, focusKey } = useFocusable()
 
   if (!playingMovie && !playingShow) {
     return null
   }
 
-  return <MediaVideoComponent />
+  return (
+    <FocusContext.Provider value={focusKey}>
+      <MediaVideoComponent ref={ref} />
+    </FocusContext.Provider>
+  )
 }
